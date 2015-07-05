5c3384256bb3fbfef57e536b5e2c2767e1dc4895
Morton Jonuschat <yabawock@gmail.com>
2015-07-05 15:29:31 +0200

    Add basic PageTS setup

--- ext_tables.php
+++ ext_tables.php
@@ -8,3 +8,10 @@ if (!defined('TYPO3_MODE')) {
 	$_EXTKEY, 'Configuration/TypoScript',
 	'SitePackage Starter'
 );
+
+// THINK BEFORE UNCOMMENTING THE FOLLOWING LINES - THEY WILL INFLUENCE
+// ALL SITES IN THE TYPO3 CMS INSTALLATION!
+// \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+//	'<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY .
+//	'/Configuration/PageTS/setup.txt">'
+// );
