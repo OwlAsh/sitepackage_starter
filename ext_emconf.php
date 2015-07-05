7b3ddafa7ccb062489048c58f9c3b497142f7a24
Morton Jonuschat <yabawock@gmail.com>
2015-07-05 15:26:44 +0200

    Add basic TypoScript setup

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,9 @@ $EM_CONF[$_EXTKEY] = array(
 	'author_company' => 'MoJo Code',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.2.0-7.99.99'
+			'typo3' => '6.2.0-7.99.99',
+			'css_styled_content' => '6.2.99-7.9.99',
+			'realurl' => '1.12.8-1.13.99',
 		),
 		'conflicts' => array(),
 		'suggests' => array()
