e65e129b53bd9c3ce1779e05396f1171923580c6
Morton Jonuschat <yabawock@gmail.com>
2015-07-08 22:09:02 +0200

    Enable BrowserSync Proxy

--- gulpfile.js/config/browserSync.js
+++ gulpfile.js/config/browserSync.js
@@ -3,5 +3,6 @@ var config = require('./');
 module.exports = {
     server: false,
     open: false,
+    proxy: config.browserSyncTarget,
     files: [config.sourceDirectory + '/{Layouts,Partials,Templates}/**/*.html']
 };
