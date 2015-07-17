666e4a645fee0cc1aee16c1345ae03829c24f551
Morton Jonuschat <yabawock@gmail.com>
2015-07-17 12:38:15 +0200

    [TASK] Rename StyleSheets folder to Css

--- gulpfile.js/config/sass.js
+++ gulpfile.js/config/sass.js
@@ -8,8 +8,8 @@ module.exports = {
             'Firefox ESR'
         ]
     },
-    src: config.assetDirectory + "/StyleSheets/**/*.{sass,scss}",
-    dest: config.publicDirectory + '/StyleSheets',
+    src: config.assetDirectory + "/Css/**/*.{sass,scss}",
+    dest: config.publicDirectory + '/Css',
     settings: {
         precision: 10,
         includePaths: [config.bowerDirectory]
